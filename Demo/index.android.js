7ae9a2f016d68a25f7bc6da4257f0bd90490e732
Wayne Walrath <wayne@evolvingmedia.net>
2017-02-20 20:24:42 -0500

    Updated gradle settings to get project to compile with Aeris SDK included in build -- TRANSFER THESE MODS TO THE standalone native module later - These are in the "Demo" project which includes the RN Native module java code. So the build parameters only apply to the demo app.

--- Demo/index.android.js
+++ Demo/index.android.js
@@ -18,7 +18,7 @@ import AerisMapsRn from './AerisMapsRn';
 
 export default class Demo extends Component { // eslint-disable-line react/prefer-stateless-function
   static onButtonPress() {
-    AerisMapsRn.show('AerisMapsRn runs fine', null); // AerisMapsRn.LONG
+    AerisMapsRn.show('AerisMapsRn: Rock-n-Roll !', AerisMapsRn.SHORT);
   }
 
   render() {
